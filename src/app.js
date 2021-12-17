0db3e495d7f361e06b7446039180b7ed9c4ef195
lysanney <93539047+lysanney@users.noreply.github.com>
2021-12-17 18:17:38 +0100

    Installed Axios + live data
    Temperature and description are now live data.

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "1ef9ad6537c2a620f87ed2a337a75fcb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
